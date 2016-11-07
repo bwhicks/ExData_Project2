656f7cb2562d6c0c3fe5007b783e5a50dd0822b7
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2016-11-06 19:23:01 -0500

    Fix plot4 to use more readable scale

--- plot4.R
+++ plot4.R
@@ -14,12 +14,17 @@ coal_comb_NEI <- NEI[NEI$SCC %in% SCC_Codes,]
 # Aggregate the sums for the entire US
 aggregated <- aggregate(Emissions~year, sum, data=coal_comb_NEI)
 
+# Round for clealiness
+aggregated$Emissions <- as.numeric(lapply(aggregated$Emissions, function (x) {
+  round(x/1000)
+}))
+
 # Make a plot using GG and bar graphs, call PNG device to write
-png(filename='plot4.png', height=480, width=960)
+dev.copy(png, filename='plot4.png', height=480, width=960)
 ggplot(data=aggregated, aes(x=year, y=Emissions)) +
   labs(title='US Coal Combustion Related PM2.5, 1999-2005',
     x='Year',
-    y='PM2.5 (tons)'
+    y='PM2.5 (thousands of tons)'
   ) +
   geom_bar(stat="identity") +
   scale_x_continuous(breaks=c(1999, 2002, 2005, 2008), labels=c('1999', '2002',
